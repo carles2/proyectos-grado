d38d20720135975a45abb19d014b3616618cb136
enmica04@alumnos.unex.es <enmica04@alumnos.unex.es@c8882eb6-480e-0940-246a-d070be4cbcd2>
2012-10-06 19:26:56 +0000

    modificaciones menores, fallos de inicializaon de variables, boolean invertidos..
    version completamente funcional

--- trunk/Apalabrados2/src/Apalabrados.java
+++ trunk/Apalabrados2/src/Apalabrados.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Apalabrados {
 
 	/**
@@ -7,33 +9,54 @@ public class Apalabrados {
 		Tablero tablero;
 		Jugador jugador1,jugador2;
 		Bolsa bolsa;
-		
 		Diccionario diccionario;
+		Random r;
+		
 		diccionario = new Diccionario();
-	
+		r = new Random();
 		bolsa = new Bolsa();
 
 		tablero= new Tablero();
 		jugador1 = new Jugador("Enrique",bolsa);
 		jugador2 = new Jugador("Jose",bolsa);
 		
+		
 		//realiza el juego mientras queden fichas
 		tablero.pintaTablero();
-		do{
-			jugador1.pintaFichas();
-			jugador1.realizaTurno(tablero.getTablero(),bolsa,diccionario);
-			// pinta la puntuacion
-			System.out.println("puntuacion del jugador "+jugador1.getNombreJugador()+": "+jugador1.getPuntuacion()+" puntos");
 
-			tablero.pintaTablero();
-			
-			jugador2.pintaFichas();
-			jugador2.realizaTurno(tablero.getTablero(),bolsa,diccionario);
+		if (r.nextFloat() >=0.5)			
+			do{
+				jugador1.pintaFichas();
+				jugador1.realizaTurno(tablero,bolsa,diccionario);
+				// pinta la puntuacion
+				System.out.println("puntuacion del jugador "+jugador1.getNombreJugador()+": "+jugador1.getPuntuacion()+" puntos");
 
-			// pinta la puntuacion
-			System.out.println("puntuacion del jugador "+jugador2.getNombreJugador()+": "+jugador2.getPuntuacion()+" puntos");
+				tablero.pintaTablero();
 			
-			tablero.pintaTablero();
-		}while (bolsa.getQuedanFichas());
+				jugador2.pintaFichas();
+				jugador2.realizaTurno(tablero,bolsa,diccionario);
+
+				// pinta la puntuacion
+				System.out.println("puntuacion del jugador "+jugador2.getNombreJugador()+": "+jugador2.getPuntuacion()+" puntos");
+			
+				tablero.pintaTablero();
+			}while (bolsa.getQuedanFichas());
+		else
+			do{
+				jugador2.pintaFichas();
+				jugador2.realizaTurno(tablero,bolsa,diccionario);
+				// pinta la puntuacion
+				System.out.println("puntuacion del jugador "+jugador2.getNombreJugador()+": "+jugador2.getPuntuacion()+" puntos");
+
+				tablero.pintaTablero();
+			
+				jugador1.pintaFichas();
+				jugador1.realizaTurno(tablero,bolsa,diccionario);
+
+				// pinta la puntuacion
+				System.out.println("puntuacion del jugador "+jugador1.getNombreJugador()+": "+jugador1.getPuntuacion()+" puntos");
+			
+				tablero.pintaTablero();
+			}while (bolsa.getQuedanFichas());	
 	}
 }
